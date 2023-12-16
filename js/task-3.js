6456d5f5a449b50fdf459a38bd550e4d6478d01b
VladyslavHn <vlad.gnatok@gmail.com>
2023-12-16 02:50:46 +0100

    hw06-js-markup

--- js/task-3.js
+++ js/task-3.js
@@ -1,26 +1,26 @@
 class StringBuilder {
-  #value;
+     #value;
 
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
+     constructor(initialValue) {
+          this.#value = initialValue;
+     }
 
-  getValue() {
-    return this.#value;
-  }
+     getValue() {
+          return this.#value;
+     }
 
-  padEnd(str) {
-    this.#value += str;
-  }
+     padEnd(str) {
+          this.#value += str;
+     }
 
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
+     padStart(str) {
+          this.#value = str + this.#value;
+     }
 
-  padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
-  }
+     padBoth(str) {
+          this.padStart(str);
+          this.padEnd(str);
+     }
 }
 
 const builder = new StringBuilder(".");
